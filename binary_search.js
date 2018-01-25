0f68baf9d48a54a1772bafc032b380cc1b73e73b
Ayush Kumar <ayushkumar26@gmail.com>
2018-01-24 18:04:41 -0800

    implement binary search with passing test cases

--- binary_search.js
+++ binary_search.js
@@ -1,15 +1,24 @@
 // binary search algorithm on an sorted array
 // return index of target if found, otherwise return -1
 
-function binarySearch(arr, target) {
-  // check if middle element of array equals target
-    // take the greater of two elements in the case of an even length array
-  // if so, return index of current (middle) element
-  // if not, check if target is greater or less than middle element
-      // if target is greater, call binarySearch on larger half of array
-      // if target is less, call binarySearch on smaller half of array
+function binarySearchRecurse(arr, target, low, high) {
+  if (low > high) {
+    return -1;
+  }
 
-  const currentIdx = Math.ceil((arr.length - 1) / 2);
+  let mid = low + Math.floor((high - low) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    return binarySearchRecurse(arr, target, mid + 1, high);
+  } else {
+    return binarySearchRecurse(arr, target, low, mid - 1);
+  }
+}
+
+function binarySearch(arr, target) {
+  return binarySearchRecurse(arr, target, 0, (arr.length - 1));
 }
 
 /* TEST CASES */
@@ -20,5 +29,8 @@ const testArrEven = [1, 2, 3, 4, 5, 6, 7, 8];
 const targetOdd = 3;
 const targetEven = 10;
 
-binarySearch(testArrOdd, targetOdd); // --> 2
-// binarySearch(testArrEven, targetEven); // --> -1
+const testOddLength = binarySearch(testArrOdd, targetOdd); // --> 2
+const testEvenLength = binarySearch(testArrEven, targetEven); // --> -1
+
+console.log(testOddLength);
+console.log(testEvenLength);

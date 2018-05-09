a248d45f7d28a6bf7210d2493cc5cb0923cfc80d
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-09 16:17:50 -0700

    sort array but not return value correctly

--- bubble_sort.js
+++ bubble_sort.js
@@ -6,6 +6,23 @@ function bubbleSort(arr) {
     // if so, swap both elements
     // increment swap counter
   // once at end of arr, if swaps > 0, sort arr again
+
+  var swaps = 0; 
+  
+  for (var i = 0; i < arr.length; i += 1) {
+    if (arr[i + 1] < arr[i]) {
+      var temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+      swaps += 1;
+    }
+  }
+
+  if (swaps === 0) {
+    return arr;
+  }
+  
+  bubbleSort(arr);
 }
 
 // TESTS:

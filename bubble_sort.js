4533ad2853aa9d312ecad51f07c4fb174ecc8786
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-09 16:10:39 -0700

    implement pseudocode

---
+++ bubble_sort.js
@@ -0,0 +1,17 @@
+// Implement the bubble sort algorithm on an array of integers
+
+function bubbleSort(arr) {
+  // start swap counter at 0
+  // iterate through the arr and check if e + 1 < e
+    // if so, swap both elements
+    // increment swap counter
+  // once at end of arr, if swaps > 0, sort arr again
+}
+
+// TESTS:
+
+const testArr0 = [0, 7, 2, 1, 6, 4, 5, 3];
+
+const test0 = bubbleSort(testArr0);
+
+console.log(test0);

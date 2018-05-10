674d4350574a2e8a24da696219593ab5dbcc577e
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-09 17:19:53 -0700

    implement bubble sort iteratively

--- bubble_sort.js
+++ bubble_sort.js
@@ -29,6 +29,34 @@ function bubbleSortRecurse(arr) {
   return arr;
 }
 
+function bubbleSortIterate(arr) {
+  // keep a swap counter variable
+  // iterate arr and check if next element < current element
+    // if so, swap both elements
+    // change increment swapped counter
+  // if at end of array and there's been a swap, sort arr again
+  // if at end of array and no swaps, return arr
+
+  var swapped = true;
+
+  while (swapped) {
+    var swaps = 0;
+    for (var i = 0; i < arr.length; i += 1) {
+      if (arr[i + 1] < arr[i]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swaps += 1;
+      }
+    }
+    if (swaps === 0) {
+      swapped = false;
+    }  
+  }
+
+  return arr;
+}
+
 // TESTS:
 
 // recursive solution: 
@@ -38,3 +66,11 @@ const testArr0 = [0, 7, 2, 1, 6, 4, 5, 3];
 const test0 = bubbleSortRecurse(testArr0);
 
 console.log(test0);
+
+// iterative solution:
+
+const testArr1 = [0, 7, 2, 1, 6, 4, 5, 3];
+
+const test1 = bubbleSortIterate(testArr1);
+
+console.log(test1);

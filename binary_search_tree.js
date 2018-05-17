d1b1d28950f5ba1b029f3402db39f38e58b44be3
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-16 23:45:08 -0700

    create node and bst class

---
+++ binary_search_tree.js
@@ -0,0 +1,19 @@
+// implement a binary search tree
+
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data) {
+
+  }
+}

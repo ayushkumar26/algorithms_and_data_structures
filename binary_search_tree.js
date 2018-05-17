6c4b7c9894c81488782dc57d9a7d1a86698f36fe
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-17 00:17:14 -0700

    implement find min, max methods and pseudocode

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -15,24 +15,29 @@ class BinarySearchTree {
 
   add(data) {
     const node = this.root;
-
+    // if the root is null, i.e., no tree exists yet, simply add node to root
     if (node === null) {
       this.root = new Node(data);
       return;
     } else {
+      // create helper function to search tree
       const searchTree = function(node) {
         if (data < node.data) {
+          // if left child is null, node belongs there
           if (node.left === null) {
             node.left = new Node(data);
             return;
           } else if (node.left !== null) {
+            // if there is already a child in place, call searchTree on that child
             return searchTree(node.left);
           }
         } else if (data > node.data) {
+          // if right child is null, node belongs there
           if (node.right === null) {
             node.right = new Node(data);
             return;
           } else if (node.right !== null) {
+            // if there is already a child in place, call searchTree on that child
             return searchTree(node.right);
           }
         } else {
@@ -42,6 +47,24 @@ class BinarySearchTree {
 
       return searchTree(node);
     }
+  }
+
+  findMin() {
+    let current = this.root;
+    // keep looking to left child as min element will be left-most element of a BST
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  findMax() {
+    let current = this.root;
+    // keep looking to right child as max element will be right-most element of a BST
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
   }
 }
 

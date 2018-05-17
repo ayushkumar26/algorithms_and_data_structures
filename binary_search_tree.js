e806f97f99f05f667bf0f37c7119f5de694c0b86
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-16 23:59:08 -0700

    implement bst add method

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -14,6 +14,57 @@ class BinarySearchTree {
   }
 
   add(data) {
+    const node = this.root;
+
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function(data) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      
+      return searchTree(data);
+    }
+  }
+}
+
+BinarySearchTree.prototype.add = function(data) {
+  const node = this.root;
+  // check if root node exists, if not, set passed in node to root node
+  if (node === null) {
+    this.root = new Node(data);
+    return;
+  } else {
+    // if root node exists, we have to find where in the tree to place the new node
+    function searchTree(node) {
+      // search left side
+      if (data < node.data) {
+        if (!node.left) {
+          node.left = new Node(data);
+          return;
+        } else {
+          searchTree(node.left);
+        }
+      }
+    }
 
   }
+
 }

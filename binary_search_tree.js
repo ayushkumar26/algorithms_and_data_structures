d6b482b19b10d7c3fecfebd73a5cb910ad32f22a
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-17 00:33:37 -0700

    implement bst find method

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -66,4 +66,20 @@ class BinarySearchTree {
     }
     return current.data;
   }
+
+  find(data) {
+    // this method returns the entire node if the data exists in the tree
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return null;
+      }
+    }
+    return current;
+  }
 }

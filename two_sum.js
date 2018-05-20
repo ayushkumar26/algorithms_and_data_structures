2bd2837905193ffc845c562a8d3c91c305eb2516
Ayush Kumar <ayushkumar26@gmail.com>
2018-05-19 17:04:37 -0700

    refactor to es6

--- two_sum.js
+++ two_sum.js
@@ -1,11 +1,11 @@
 // given an array of integers, return the indices of the pair that sum to target
 // can not use the same index twice
 
-var twoSum = function(nums, target) {
-  var hash = {};
+const twoSum = (nums, target) => {
+  const hash = {};
 
-  for (var i = 0; i < nums.length; i += 1) {
-    var solution = target - nums[i];
+  for (let i = 0; i < nums.length; i += 1) {
+    let solution = target - nums[i];
 
     if (hash[solution] !== undefined && hash[solution] !== i) {
       return [hash[solution], i];
